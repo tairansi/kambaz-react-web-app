e104ef6a4d65a2c8eaac5adcabaaa44f61cfc0ba
Tairan Si <timothysi917@gmail.com>
2025-01-22 22:26:23 -0500

    second commit

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -19,8 +19,6 @@ export default function Courses() {
                             <Route path="/" element={<Navigate to="Home" />} />
                             <Route path="Home" element={<Home />} />
                             <Route path="Modules" element={<Modules />} />
-                            <Route path="Assignments" element={<h2>Assignments</h2>} />
-                            <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
                             <Route path="People" element={<h2>People</h2>} />
                             <Route path="Assignments" element={<Assignments />} />
                             <Route path="Assignments/:aid" element={<AssignmentEditor />} />
