9f0606bbfd16f8c1495ed7f2db1048e900541188
Tairan Si <timothysi917@gmail.com>
2025-02-20 00:56:30 -0500

    final commit

--- src/Kambaz/Database/index.ts
+++ src/Kambaz/Database/index.ts
@@ -2,5 +2,6 @@ import courses from "./courses.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
 import users from "./users.json"
-export default {
-  courses, modules, assignments, users};
+import enrollments from "./enrollments.json"
+export {
+  courses, modules, assignments, users, enrollments};
