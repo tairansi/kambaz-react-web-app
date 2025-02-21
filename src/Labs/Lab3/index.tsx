23b27be4a68f329fee376d22e9349b55b4fc3c00
Tairan Si <timothysi917@gmail.com>
2025-02-21 18:02:39 -0500

    commit late

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,37 @@
 import Add from "./Add";
-import Square from "./Square";
+
 import VariablesAndConstants from "./VariablesAndConstants";
 import Highlight from "./Highlight";
+import VariableTypes from "./VariableTypes";
+import TernaryOperator from "./TernaryOperator";
+import TemplateLiterals from "./TemplateLiterals";
+import Styles from "./Styles";
+import Spreading from "./Spreading";
+import SimpleArrays from "./SimpleArrays";
+import PathParameters from "./PathParameters";
+
+import Square from "./Square";
+import MapFunction from "./MapFunction";
+import LegacyFunctions from "./LegacyFunctions";
+import JsonStringify from "./JsonStringify";
+import ImpliedReturn from "./ImpliedReturn";
+import IfElse from "./IfElse";
+import House from "./House";
+import FunctionDestructing from "./FunctionDestructing";
+import ForLoops from "./ForLoops";
+import FindIndex from "./FindIndex";
+import FindFunction from "./FindFunction";
+import FilterFunction from "./FilterFunction";
+import DestructingImports from "./DestructingImports";
+import Destructing from "./Destructing";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import Classes from "./Classes";
+import BooleanVariables from "./BooleanVariables";
+import ArrowFunctions from "./ArrowFunctions";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddPathParameters from "./AddPathParameters";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 
 export default function Lab3() {
   console.log("Hello World!")
@@ -9,7 +39,34 @@ export default function Lab3() {
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <VariablesAndConstants/>
-
+      <VariableTypes/>
+      <TernaryOperator/>
+      <TemplateLiterals/>
+      <Styles/>
+      <Spreading/>
+      <SimpleArrays/>
+      <PathParameters/>
+      <MapFunction/>
+      <LegacyFunctions/>
+      <JsonStringify/>
+      <ImpliedReturn/>
+      <IfElse/>
+      <House/>
+      <FunctionDestructing/>
+      <ForLoops/>
+      <FindIndex/>
+      <FindFunction/>
+      <FilterFunction/>
+      <DestructingImports/>
+      <Destructing/>
+      <ConditionalOutputInline/>
+      <ConditionalOutputIfElse/>
+      <Classes/>
+      <BooleanVariables/>
+      <ArrowFunctions/>
+      <ArrayIndexAndLength/>
+      <AddPathParameters/>
+      <AddingAndRemovingToFromArrays/>
       <Add a={3} b={4} />
       <h4>Square of 4</h4>
       <Square>4</Square>
