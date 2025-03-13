f659786e101dfa541c90dba79cf9b177146dfd47
tairansi <timothysi917@gmail.com>
2025-03-13 01:15:32 -0400

    first

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -32,12 +32,23 @@ import ArrowFunctions from "./ArrowFunctions";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import AddPathParameters from "./AddPathParameters";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import { ListGroup } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 export default function Lab3() {
+  const { todos } = useSelector((state: any) => state.todosReducer);
   console.log("Hello World!")
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
+      <ListGroup>
+        {todos.map((todo: any) => (
+          <ListGroup.Item key={todo.id}>
+            {todo.title}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <hr />
       <VariablesAndConstants/>
       <VariableTypes/>
       <TernaryOperator/>
